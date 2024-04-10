15dcb9120d149f189f85ec313669b00c073630b3
John Westhoff <johnjwesthoff@gmail.com>
2024-04-10 07:29:22 -0700

    Updoots

--- undo.gd
+++ undo.gd
@@ -1,8 +1,8 @@
-extends Node2D
+extends Control
 
 func _input(event):
 	if event is InputEventMouseButton:
-		if event.is_pressed() and get_local_mouse_position().length() < 48:
+		if event.is_pressed() and get_local_mouse_position().length() < 64:
 			Global.undo.emit()
 			return false
 	return true
