f64703ac69b2b4484ed6ae63781384da9450b075
John Westhoff <johnjwesthoff@gmail.com>
2024-10-09 22:10:20 -0700

    Start autobattler

---
+++ Autobattler/minion.gd
@@ -0,0 +1,31 @@
+class_name Minion extends Node2D
+
+var health: int = 2
+var attack: int = 2
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
+
+func _premove():
+	pass
+	
+func _move():
+	pass
+	
+func _postmove():
+	pass
+	
+func _predie():
+	pass
+	
+func _prematch():
+	pass
+	
+func _firstturn():
+	pass
